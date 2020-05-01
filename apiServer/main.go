79958c06e32b3860de8575353e50fa841e2182b6
Suzuki Takeo <takeo@stko.info>
2020-05-01 15:20:05 +0900

    swagger関連のコード追加

--- apiServer/main.go
+++ apiServer/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/BambooTuna/gae-project-template/apiServer/interfaces"
+	"github.com/BambooTuna/gae-project-template/apiServer/swagger/docs"
 	"github.com/BambooTuna/go-server-lib/config"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"log"
 )
 
@@ -15,12 +18,12 @@ func main() {
 	sampleHandler := interfaces.SampleHandler{}
 	r.POST("/sample", sampleHandler.PostSampleRoute())
 
-	//if gin.Mode() == gin.DebugMode {
-	//	docs.SwaggerInfo.Schemes = []string{"http"}
-	//	docs.SwaggerInfo.Host = fmt.Sprintf("localhost:%s", serverPort)
-	//	docs.SwaggerInfo.BasePath = ""
-	//	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	//}
+	if gin.Mode() == gin.DebugMode {
+		docs.SwaggerInfo.Schemes = []string{"http", "https"}
+		docs.SwaggerInfo.Host = ""
+		docs.SwaggerInfo.BasePath = ""
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 
 	log.Fatal(r.Run(fmt.Sprintf(":%s", serverPort)))
 }
